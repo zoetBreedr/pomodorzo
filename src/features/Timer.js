a95bc9bfe8b017434f6e0067a0783e16f89594c7
Zoe Thexton <zoe.thexton@breedr.co>
2023-06-12 14:43:56 +0100

    Add progress bar

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,19 +1,34 @@
 import { StyleSheet, View, Text } from "react-native";
+import { ProgressBar } from "react-native-paper";
 import { Countdown } from "../components/Countdown";
-import { spacing } from "../utils/sizes";
+import { spacing, fontSizes } from "../utils/sizes";
+import { colours } from "../utils/colours";
 import { RoundedButton } from "../components/RoundedButton";
 import { useState } from "react";
 
 export const Timer = ({ subject }) => {
 	const [isStarted, setIsStarted] = useState(false);
+	const [progress, setProgress] = useState(1);
 
 	return (
 		<View style={styles.container}>
 			<View style={styles.countdown}>
 				<Countdown
 					isPaused={!isStarted}
-					onProgress={() => {}}
+					onProgress={(progress) => {
+						setProgress(progress);
+					}}
 					onTimerEnd={() => {}}
+				/>
+				<View style={{ paddingTop: spacing.xxl }}></View>
+				<Text style={styles.title}>Focusing on:</Text>
+				<Text style={styles.task}>{subject}</Text>
+			</View>
+			<View style={{ paddingTop: spacing.sm }}>
+				<ProgressBar
+					color={colours.lightBlue}
+					progress={progress}
+					style={{ height: spacing.sm }}
 				/>
 			</View>
 			<View style={styles.buttonWrapper}>
@@ -35,6 +50,17 @@ const styles = StyleSheet.create({
 		flex: 0.5,
 		alignItems: "center",
 		justifyContent: "center",
+	},
+	title: {
+		color: colours.white,
+		fontWeight: "bold",
+		textAlign: "center",
+		fontSize: fontSizes.lg,
+	},
+	task: {
+		color: colours.white,
+		textAlign: "center",
+		fontSize: fontSizes.md,
 	},
 	buttonWrapper: {
 		flex: 0.3,
--- src/utils/colours.js
+++ src/utils/colours.js
@@ -1,5 +1,6 @@
 export const colours = {
-    white: '#fff',
-    black: '#000',
-    darkBlue: '#252250',
-}
+	white: "#fff",
+	black: "#000",
+	darkBlue: "#252250",
+	lightBlue: "#5e84e2",
+};
